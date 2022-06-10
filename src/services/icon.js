ca7124b59b7ad26575490f268ac6912c2b07826d
DaVinci <prazzeettstha@gmail.com>
2022-06-10 11:39:45 +0545

    icon cleaned

--- src/services/icon.js
+++ src/services/icon.js
@@ -3,28 +3,20 @@
 const IconService = require("icon-sdk-js").default;
 const HttpProvider = IconService.HttpProvider;
 
-const WALLET_NETWORK = "https://sejong.net.solidwallet.io/api/v3";
-const WALLET_NETWORK_BERLIN = "https://berlin.net.solidwallet.io/api/v3";
-const WALLET_NETWORK_LISBON = "https://lisbon.net.solidwallet.io/api/v3";
-
 const WALLET_NETWORK_MAIN = "https://ctz.solidwallet.io/api/v3d";
+const WALLET_NETWORK_SEJONG = "https://sejong.net.solidwallet.io/api/v3d";
+const WALLET_NETWORK_BERLIN = "https://berlin.net.solidwallet.io/api/v3d";
+const WALLET_NETWORK_LISBON = "https://lisbon.net.solidwallet.io/api/v3d";
 
-const WALLET_NETWORK_d = "https://sejong.net.solidwallet.io/api/v3d";
-const WALLET_NETWORK_BERLIN_d = "https://berlin.net.solidwallet.io/api/v3d";
-const WALLET_NETWORK_LISBON_d = "https://lisbon.net.solidwallet.io/api/v3d";
-
-const provider_d = new HttpProvider(WALLET_NETWORK_d);
-const provider = new HttpProvider(WALLET_NETWORK);
+const provider_sejong = new HttpProvider(WALLET_NETWORK_SEJONG);
 const provider_mainnet = new HttpProvider(WALLET_NETWORK_MAIN);
-const provider_berlin = new HttpProvider(WALLET_NETWORK_BERLIN_d);
-const provider_lisbon = new HttpProvider(WALLET_NETWORK_LISBON_d);
+const provider_berlin = new HttpProvider(WALLET_NETWORK_BERLIN);
+const provider_lisbon = new HttpProvider(WALLET_NETWORK_LISBON);
 
-const iconService_d = new IconService(provider_d);
-const iconService_d_mainnet = new IconService(provider_mainnet);
-const iconService_d_berlin = new IconService(provider_berlin);
-const iconService_d_lisbon = new IconService(provider_lisbon);
-
-const iconService = new IconService(provider);
+const iconService_sejong = new IconService(provider_sejong);
+const iconService_mainnet = new IconService(provider_mainnet);
+const iconService_berlin = new IconService(provider_berlin);
+const iconService_lisbon = new IconService(provider_lisbon);
 
 export const getTx = async (
   txHash,
@@ -39,13 +31,13 @@ export const getTx = async (
     console.log(txHash, mainnet, sejong, berlin, lisbon, custom, CustomServer);
     let res = "";
     if (mainnet) {
-      res = await iconService_d_mainnet.getTrace(txHash).execute();
+      res = await iconService_mainnet.getTrace(txHash).execute();
     } else if (sejong) {
-      res = await iconService_d.getTrace(txHash).execute();
+      res = await iconService_sejong.getTrace(txHash).execute();
     } else if (berlin) {
-      res = await iconService_d_berlin.getTrace(txHash).execute();
+      res = await iconService_berlin.getTrace(txHash).execute();
     } else if (lisbon) {
-      res = await iconService_d_lisbon.getTrace(txHash).execute();
+      res = await iconService_lisbon.getTrace(txHash).execute();
     } else if (custom) {
       if (CustomServer.length >= 1) {
         const WALLET_NETWORK_CUSTOM = CustomServer;
